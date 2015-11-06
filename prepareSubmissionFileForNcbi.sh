6f923fa70fb8a66addd3277a7072e36dae83c49f
Hakim Tafer <htafer@gmail.com>
2015-11-06 15:55:26 +0100

    Update and rename prepareSubmissionFileForNcbi.pl to prepareSubmissionFileForNcbi.sh

--- prepareSubmissionFileForNcbi.sh
+++ prepareSubmissionFileForNcbi.sh
@@ -10,28 +10,28 @@ while [ "$1" != "" ]; do
     case $1 in
         -s | --swissProtBLAST ) shift
                                 swissProtBLAST=$1
-                                ;;   
+                                ;;
         -f | --functional)      shift
                                 functional=$1
-                                ;;   
+                                ;;
         -S | --swissProtFASTA)  shift
                                 swissProtFASTA=$1
-                                ;;   
+                                ;;
         -N | --names)           shift
                                 names=$1
-                                ;;   
+                                ;;
         -F | --genomeSeq)       shift
                                 genomeSeq=$1
-                                ;;   
+                                ;;
         -G | --genomeAnn)       shift
                                 genomeAnn=$1
-                                ;;   
+                                ;;
         -a | --annPrefix)       shift
                                 annPrefix=$1
-                                ;;   
+                                ;;
         -h | --help )           usage
                                 exit
-                                ;;   
+                                ;;
         * )                     usage
                                 exit 1
     esac
@@ -45,11 +45,6 @@ echo "cat $genomeAnn | enumerateCDS.pl | testFeatures2.pl -F $genomeSeq -g /dev/
 cat $genomeAnn | enumerateCDS.pl | testFeatures2.pl -F $genomeSeq -g /dev/stdin -a $annPrefix -f $functional -n $names > $tblFile
 
 printf "Now you are ready to pass the .tbl and .fsa file to linux64.tbl2asn\n"
-printf "linux64.tbl2asn -t template.sbt -p . -a s -V v -j \"[organism=Debaryomyces fabryi][strain=CBS789]\" -i $genomeSeq"
-
-
-
-
-
-
+printf "linux64.tbl2asn -p .  -t template.sbt -M n -Z discrep -j \"[organism=Debaryomyces fabryi][strain=CBS789]\"                                                                                                                                                               
+                                                                                                                                                                                                                                                                                 
 
